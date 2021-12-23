8958089eea1c530522fbb4b2034d986be8b1febd
YuraPaisanidi <yura.paisanidi@gmail.com>
2021-12-23 12:30:50 +0200

    first commit

---
+++ youbor/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+	<meta charset="utf-8">
+	<title>youbor.uz</title>
+	<meta name="description" content="youbor.uz">
+	<meta name="viewport" content="width=device-width">
+	<link rel="icon" href="img/favicon.ico">
+	<!-- <meta property="og:image" content="img/preview.jpg"> -->
+	<link rel="stylesheet" href="css/app.min.css?v=1">
+	<script src="js/app.min.js" defer></script>
+
+	<?php wp_head(); ?>
+
+</head>
+
+<body>
+
+	<?php get_template_part( 'parts/header' ); ?>
+
+	
